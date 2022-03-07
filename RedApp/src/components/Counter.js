5f044bfef085e65f4087a239c14f7ff8e6e8ec23
Kargyrop <kargyr.ankronet@gmail.com>
2022-03-07 19:01:07 +0200

    Finished counterApp

--- RedApp/src/components/Counter.js
+++ RedApp/src/components/Counter.js
@@ -2,26 +2,34 @@ import { useSelector, useDispatch } from 'react-redux';
 //import {connect} from 'react-redux';
 //import {Component} from 'react';
 import classes from './Counter.module.css';
+import { counterActions } from '../store/index';
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({type: 'increment'});
+    dispatch(counterActions.increment());
+  };
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5)); // {type: SOME_UNIQUELY_GENERATED_IDENTIFIER, payload: 10}
   };
   const decrementHandler = () => {
-    dispatch({type: 'decrement'});
+    dispatch(counterActions.decrement());
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
       return (
       <main className={classes.counter}>
         <h1>Redux Counter</h1>
-        <div className={classes.value}>{counter}</div>
+        {show && <div className={classes.value}>{counter}</div>}
         <div>
           <button onClick={incrementHandler}>increment</button>
+          <button onClick={increaseHandler}>increase by 5</button>
           <button onClick={decrementHandler}>decrement</button>
         </div>
         <button onClick={toggleCounterHandler}>Toggle Counter</button>
